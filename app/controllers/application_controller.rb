8bfed7237d9a139e8346c1c4ec1bcc7af75ab3aa
Bryce Eadie <bryce.eadie@gmail.com>
2018-11-22 06:58:56 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,20 @@ class ApplicationController < Sinatra::Base
   get '/' do
     erb :"application/index"
   end
+
+  patch '/figures/:id' do
+    @figure = Figure.find_by_id(params[:id])
+    @figure.update(params[:figure])
+    if params[:landmark][:name]
+      landmark = Landmark.create(name: params[:landmark][:name])
+      @figure.landmarks << landmark
+    end
+    if params[:title][:name]
+      title = Title.create(name: params[:title][:name])
+      @figure.titles << title
+    end
+    @figure.save
+    redirect 'figures/#{figure.id}'
+  end
+
 end
